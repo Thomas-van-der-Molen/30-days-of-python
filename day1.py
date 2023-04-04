3e05e4ffe2392fd3583e04df8c7f2b95449cb3f9
Thomas <58376739+Thomas-van-der-Molen@users.noreply.github.com>
2023-04-04 14:14:53 -0400

    Complete Day1: introduction

---
+++ day1.py
@@ -0,0 +1,26 @@
+#I am using python version 3.8.10
+
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+
+print("Name: Tom")
+print("Family name: VDM")
+print("Country: United States")
+print("I am enjoying 30 days of python")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['asabeneh','python','finland']))
+
+#the points are (2,3) and (10,8)
+eucDistance = (((8-3)**2) + ((10-2)**2) **.5)
+print(f'The euclidean distance between (2,3) and (10,8) is {eucDistance}')
