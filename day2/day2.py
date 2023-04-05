dab5a1655b1f8ba57c42128fd7b63c652d3bffc4
Thomas <58376739+Thomas-van-der-Molen@users.noreply.github.com>
2023-04-05 08:04:58 -0400

    Complete day2: variables and built-in functions

---
+++ day2/day2.py
@@ -0,0 +1,46 @@
+#day 2: 30 days of python programming
+
+first_name = "Thomas"
+last_name = "VDM"
+full_name = first_name + last_name
+country = "United States"
+age = 250
+year = 2023
+is_married = False
+is_true = True
+is_light_on = False
+
+job_goal, favorite_city = "Developer at Google", "Amsterdam"
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(job_goal))
+print(type(favorite_city))
+
+print(len(first_name))
+print(len(first_name) - len(last_name))
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one //  num_two
+
+
+radius = input("What is the radius?")
+area_of_circle = (3.14) * (int(radius) ** 2)
+circum_of_circle = 2 * 3.14 * int(radius)
+print(f'The area is {area_of_circle} and the circumference is {circum_of_circle}')
+
+
